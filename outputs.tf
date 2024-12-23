52e24d29d3d71c7527f1af98517ac081ed695810
Muller Matos <98285191+millerjmatos@users.noreply.github.com>
2024-12-22 21:05:18 -0300

    Create outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,14 @@
+output "instance_id" {
+  description = "ID of the EC2 instance"
+  value       = aws_instance.server.id
+}
+
+output "instance_public_ip" {
+  description = "Public IP address of the EC2 instance"
+  value       = aws_instance.server.public_ip
+}
+
+output "instance_public_dns" {
+  description = "Public DNS of the EC2 instance"
+  value       = aws_instance.server.public_dns
+}
